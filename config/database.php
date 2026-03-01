20832a663495b4cc4e661d712271fb6f965c02f5
Miguel Isique <milanca221@gmail.com>
2026-02-28 19:32:51 -0500

    modificar variables globales

--- config/database.php
+++ config/database.php
@@ -1,10 +1,17 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 
+use Dotenv\Dotenv;
+
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+    $dotenv->load();
+}
 return [
-    'driver'   => 'mysql',
-    'host'     => $_ENV['DB_HOST'],
-    'database' => $_ENV['DB_NAME'],
-    'username' => $_ENV['DB_USER'],
-    'password' => $_ENV['DB_PASS'],
-    'charset'  => 'utf8mb4'
+    'driver' => 'mysql',
+    'host' => $_ENV['DB_HOST'] ?? getenv('DB_HOST'),
+    'database' => $_ENV['DB_NAME'] ?? getenv('DB_NAME'),
+    'username' => $_ENV['DB_USER'] ?? getenv('DB_USER'),
+    'password' => $_ENV['DB_PASS'] ?? getenv('DB_PASS'),
+    'charset' => 'utf8mb4'
 ];
